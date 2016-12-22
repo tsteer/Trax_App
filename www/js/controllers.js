e53de0c2dda0b514d5d0ff1410f9cc1fa641a014
tsteer <tasminsteer@hotmail.com>
2016-12-22 10:42:05 +0000

    Update_Get

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,36 +1,45 @@
 
 angular.module('starter.controllers', ['ionic', 'ui.router'])
 .controller('LoginCtrl', function($scope, $state, $http, $stateParams){
-
   $scope.login = function(email){
-
+//cloud9 - ionic where set x originresources - set which url can access
     // $.post(url, data, function () {});
 //get email from email field
+var useremail = email;
+var userpass = password;
+console.log("user: " + useremail);
+console.log("user p: " + userpass);
+//"email=test@test.com&password=testpassword"
     $http({
       method: 'POST',
-      url: "http://localhost:3000/login?json=1", //req.body
-      data: "email=test@test.com", //"id=" + 11,  - email variable here
+      //url: "https://fkbokthszu.localtunnel.me/login?json=1", //req.body
+       url: "http://localhost:3000/login?json=1", //req.body
+      data: "email=" + useremail + "&password=" + "testpassword", //   "id=" + 11,  - email variable here //"id=" + 11,  - email variable here
       headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     }).success(function(res) {
       console.log("post success");
       var token = {
         token: res.token
-      };
+    
+      };  
       var id= res.id;
       console.log("check id here sent" + res.id);
-  // var id = apiToken.findUserByToken(token);
-    //  console.log("user" + id);
-            console.log("token key" + token);
-         
+      // var id = apiToken.findUserByToken(token);
+      //  console.log("user" + id);
+      console.log("token key" + token);    
       window.localStorage.setItem( 'tokenkey', token.token );
- console.log("the login token: " + token.token);
-     // console.log("the login token: " + tokenkey);
+      console.log("the login token: " + token.token);
+      // console.log("the login token: " + tokenkey);
       //$window.localStorage.setItem('token', res.token);
-     //     console.log("login 2gdfghidf " + $stateParams.token);
+      //     console.log("login 2gdfghidf " + $stateParams.token);
       $state.go('tab.account', {id: id}); // for UI
-    console.log('login', email);
-    }).error(function(err){
+      console.log('login', email);
+    }).error(function(err, p2, p3, p4, p5){
       console.log("check this: " + err);
+      //console.log("p2: " + p2);
+      //console.log("p3: " + p3);
+      //console.log("p4: " + p4);
+      //console.log("p5: " + p5);
     });
   //     url: "http://localhost:3000/login?id=1", req.params
     /*
@@ -126,17 +135,13 @@ angular.module('starter.controllers', ['ionic', 'ui.router'])
   if(window.localStorage.getItem('tokenkey')){
     var token = window.localStorage.getItem('tokenkey');
     $http.defaults.headers.common['X-Auth-Token'] = token;
- console.log($http.defaults.headers.common['X-Auth-Token']);
     $http({
       method: 'GET',
       url: "http://localhost:3000/people/1?json=1", 
     //  data: "id=" + 11, // id in url - dont need here otherwise data goe sn here eg changing
       headers: {'Content-Type': 'application/x-www-form-urlencoded', 'X-Auth-Token': token}
     }).success(function(data, status, headers,config) {
-$scope.people = data;
-      console.log("post success");
-      console.log($scope.people);
-      console.log("check2" + data.first_name);
+      $scope.people = data;
     }).error(function(err){
       console.log("check this " + err);
     });
@@ -148,17 +153,14 @@ $scope.people = data;
   if(window.localStorage.getItem('tokenkey')){
     var token = window.localStorage.getItem('tokenkey');
     $http.defaults.headers.common['X-Auth-Token'] = token;
- console.log("test " + $http.defaults.headers.common['X-Auth-Token']);
     $http({
       method: 'GET',
       url: "http://localhost:3000/edituser/1?json=1", 
     //  data: "id=" + 11, // id in url - dont need here otherwise data goe sn here eg changing
       headers: {'Content-Type': 'application/x-www-form-urlencoded', 'X-Auth-Token': token}
     }).success(function(data, status, headers,config) {
-$scope.edituser = data;
-      console.log("post success");
-      console.log($scope.edituser);
-      console.log("check2" + data.first_name);
+      $scope.edituser = data;
+      console.log(JSON.stringify(data));
     }).error(function(err){
       console.log("check this " + err);
     });
